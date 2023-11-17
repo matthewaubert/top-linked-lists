5537dbc66de4a07a293d7020eb76611131b113c0
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-11-17 16:54:13 -0500

    Build size, head funcs; fix toString bug

--- linked-list.js
+++ linked-list.js
@@ -3,46 +3,65 @@ import Node from './node.js';
 // create a new linked list
 // with appropriate functionality on prototype
 export default class LinkedList {
-  constructor(head = null) {
-    this.head = head; // pointer to start of linked list
+  constructor(listHead = null) {
+    this.listHead = listHead; // pointer to start of linked list
   }
 
   // traverse to end of list
   // #traverse() {
-  //   let tmp = this.head;
+  //   let tmp = this.listHead;
   //   while (tmp.nextNode !== null) tmp = tmp.nextNode;
   //   return tmp;
   // }
 
+  // add a new value at the start of the linked list
+  prepend(value) {
+    // create a new listHead and point it to current listHead
+    const newListHead = new Node(value, this.listHead);
+    this.listHead = newListHead; // set new listHead to be the listHead
+  }
+
   // add a new value to the end of the linked list
   append(value) {
-    if (this.head === null) {
+    // if no values yet, prepend value
+    if (this.listHead === null) {
       this.prepend(value);
     } else {
-      let tmp = this.head;
-      while (tmp.nextNode !== null) tmp = tmp.nextNode;
+      let tmp = this.listHead; // point tmp to listHead
+      while (tmp.nextNode !== null) tmp = tmp.nextNode; // traverse to end
 
-      tmp.nextNode = new Node(value);
+      tmp.nextNode = new Node(value); // add new node to end
     }
   }
 
-  // add a new value at the start of the linked list
-  prepend(value) {
-    // create a new head and point it to current head
-    const newHead = new Node(value, this.head);
-    this.head = newHead; // set new head to be the head
+  // return num of nodes in list
+  size() {
+    let count = 0;
+
+    let tmp = this.listHead; // point tmp to listHead
+    // traverse to end of list, counting each time
+    while (tmp !== null) {
+      count++;
+      tmp = tmp.nextNode;
+    }
+
+    return count;
+  }
+
+  head() {
+    return this.listHead.value;
   }
 
   toString() {
     let string = '';
 
-    let tmp = this.head;
+    let tmp = this.listHead;
     while (tmp !== null) {
       string += `( ${tmp.value} ) -> `;
       tmp = tmp.nextNode;
     }
 
-    string += 'tmp';
+    string += tmp;
     return string;
   }
 }
@@ -50,4 +69,7 @@ export default class LinkedList {
 const list = new LinkedList();
 list.append('val1');
 list.append('val2');
-console.log(list.toString());
+console.log(list.size()); // 2
+console.log(list.head()); // val1
+
+console.log(list.toString()); // ( val1 ) -> ( val2 ) -> tmp
